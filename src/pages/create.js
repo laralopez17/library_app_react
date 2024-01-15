c2d09abf33c9385928eef0cd96c307530f3f27ef
laralopez17 <laralopez219@gmail.com>
2024-01-15 20:53:49 -0300

    first commit

---
+++ src/pages/create.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function Create() {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [cover, setCover] = useState("");
+  const [intro, setIntro] = useState("");
+  const [completed, setCompleted] = useState(false);
+  const [review, setReview] = useState("");
+
+  return <div>Create</div>;
+}
