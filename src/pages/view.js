a7c540b54f0f506d20dcab8850449084f359a59d
laralopez17 <laralopez219@gmail.com>
2024-01-18 18:14:28 -0300

    lastCommit - for now

--- src/pages/view.js
+++ src/pages/view.js
@@ -1,3 +1,49 @@
+import { useAppContext } from "../store/store";
+import Layout from "../components/layout";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function View() {
-  return <div>View</div>;
+  const [item, setItem] = useState(null);
+  const params = useParams();
+  const store = useAppContext();
+
+  const itemStyles = {
+    container: {
+      display: "flex",
+      gap: "20px",
+      color: "white",
+      width: "800px",
+      margin: "0 auto",
+    },
+  };
+  useEffect(() => {
+    const book = store.getItem(params.bookId);
+    setItem(book);
+  }, []);
+
+  if (!item) {
+    return <Layout>Item not found</Layout>;
+  }
+
+  return (
+    <Layout>
+      <div style={itemStyles.container}>
+        <div>
+          {item?.cover ? (
+            <img src={item?.cover} width="400" alt={item.title} />
+          ) : (
+            ""
+          )}
+        </div>
+        <div>
+          <h2>{item?.title}</h2>
+          <div>{item?.author} </div>
+          <div>{item?.intro} </div>
+          <div>{item?.completed ? "Leído" : "Por terminar"} </div>
+          <div>{item?.review} </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
